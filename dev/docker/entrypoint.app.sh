05273e604a79fa0867a480ffd3c966144d7092ed
shankar-dangi <shankar@simformsolutions.com>
2023-04-17 13:29:54 +0530

    Install node in docker file

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -11,5 +11,7 @@ else
     wait-for-it $DB_HOST:3306 -t 45
     php artisan migrate --database=mysql --force
     chown -R www-data:www-data storage
+    npm install
+    npm run build
     exec apache2-foreground
 fi
